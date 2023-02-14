5ad0f2c905d3495ac9803186e648b79a0bab654c
Egbe Alexander <alexsucceeds@gmail.com>
2023-02-14 14:18:02 +0100

    Inital Commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,30 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const  mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
 
-const app = express()
+// express app
+const app = express();
 
-app.listen(4000, () =>{
-    console.log('listening on port 4000');
-})
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/workouts", workoutRoutes);
+
+// connect to db
+mongoose
+  .connect("mongodb://127.0.0.1:27017/workoutDB")
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port 4000");
+    });b 
+  })
+  .catch((error) => {
+    console.log(error);
+  });

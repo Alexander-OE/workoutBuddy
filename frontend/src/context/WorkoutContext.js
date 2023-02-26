1a923fe58a1ce8ad3f442cebcb661249d579293d
Egbe Alexander <alexsucceeds@gmail.com>
2023-02-26 22:12:49 +0100

    improve useReducer logic

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -2,13 +2,29 @@ import { createContext, useReducer } from "react";
 
 export const WorkoutsContext = createContext();
 
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUT":
+      return {
+        workouts: [action.payload, ...state.workouts],
+      };
+
+    default:
+      return state
+      break;
+  }
+};
+
 export const WorkoutsContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(workoutsReducer, {
-    workouts: null
+    workouts: null,
   });
 
-
-  dispatch({type: 'SET_WORKOUTS', payload: [{}, { }]})
-
-  return <WorkoutsContext.Provider value={{}}>{children}</WorkoutsContext.Provider>;
+  return (
+    <WorkoutsContext.Provider value={{state, dispatch}}>{children}</WorkoutsContext.Provider>
+  );
 };
